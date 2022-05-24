590462221d589b0c0989245f0c39c5301d33b0ca
Erwan Guiffant <ergu@mac.com>
2022-05-24 10:43:03 +0200

    feat:create_master_branch

---
+++ src/main/java/com/paymybuddy/SaP6PayMyBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.paymybuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaP6PayMyBuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaP6PayMyBuddyApplication.class, args);
+    }
+
+}
